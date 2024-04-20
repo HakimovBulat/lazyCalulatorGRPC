22e7b123e526a3c277d261fcd61681a5af13fe15
Hakimov Bulat <bulathakimov1704@gmail.com>
2024-04-20 16:30:23 +0300

    2 and 3 day

--- main.go
+++ main.go
@@ -1,33 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/HakimovBulat/lazyCalulatorGRPC/router"
+	"github.com/HakimovBulat/lazyCalulatorGRPC/utils"
 	"go.uber.org/zap"
 )
 
 func main() {
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
-	router := gin.Default()
-	router.GET("/", mainHandler)
-	router.LoadHTMLGlob("templates/*.html")
+	router := router.SetupRouter()
 	if err := router.Run(":8080"); err != nil {
-		logger.Error(err.Error(), zap.String("router", "f*ck down"))
+		utils.Logger.Error(err.Error(), zap.String("router", "f*ck down"))
 	}
-}
-func mainHandler(c *gin.Context) {
-	cookie, err := c.Request.Cookie("user")
-	fmt.Println(cookie, err)
-	if err != nil {
-		cookie = &http.Cookie{
-			Name:   "user",
-			Value:  "12345",
-			MaxAge: 300,
-		}
-		http.SetCookie(c.Writer, cookie)
-	}
-	c.HTML(200, "index.html", cookie)
 }
---
+++ router/router.go
@@ -0,0 +1,313 @@
+package router
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/HakimovBulat/lazyCalulatorGRPC/utils"
+	"github.com/apaxa-go/eval"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+type Expression struct {
+	Id            int
+	StringVersion string
+	Answer        string
+	Status        string
+	StartDate     time.Time
+	EndDate       time.Time
+}
+
+var mapOperatorsTime = map[string]int{
+	"-": 10,
+	"+": 10,
+	"*": 10,
+	"/": 10,
+}
+
+var connectionString = "host=0.0.0.0 port=5432 user=postgres password=Love_and_elephant42 dbname=Expression sslmode=disable"
+var connection *sql.DB
+
+func SetupRouter() *gin.Engine {
+	utils.SetupLogger()
+	var err error
+	connection, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		utils.Logger.Error(err.Error())
+	}
+	router := gin.Default()
+	router.GET("/", inputExpression)
+	router.POST("/", createExpression)
+	router.GET("/operators", showOperatorsTime)
+	router.PUT("/operators", replaceOperatorsTime)
+	router.GET("/static_operators", operatorsStatic)
+	router.POST("/static_operators", operatorsStatic)
+	router.GET("/get_expression/:id", getExpression)
+	router.GET("/login", getLoginHandler)
+	router.GET("/logout", logoutHandler)
+	router.POST("/login", postLoginHandler)
+	router.GET("/register", getRegisterHandler)
+	router.POST("/register", postRegisterHandler)
+	router.LoadHTMLGlob("templates/*.html")
+	return router
+}
+func logoutHandler(c *gin.Context) {
+	cookie := &http.Cookie{
+		Name:   "name",
+		MaxAge: -1}
+	http.SetCookie(c.Writer, cookie)
+}
+func getRegisterHandler(c *gin.Context) {
+	c.HTML(200, "register.html", nil)
+}
+func getLoginHandler(c *gin.Context) {
+	c.HTML(200, "login.html", nil)
+}
+func postLoginHandler(c *gin.Context) {
+	login, password := c.PostForm("login"), c.PostForm("password")
+	_, err := connection.Query(`SELECT id, name, password FROM users WHERE name=$1 AND password=$2`, login, password)
+	if err != nil {
+		utils.Logger.Error(err.Error())
+	}
+	cookie := &http.Cookie{Name: "user", Value: login, MaxAge: 300}
+	http.SetCookie(c.Writer, cookie)
+}
+func postRegisterHandler(c *gin.Context) {
+	login, password := c.PostForm("login"), c.PostForm("password")
+	_, err := connection.Query(`
+	CREATE TABLE IF NOT EXISTS "Users"(
+		"id" serial NOT NULL UNIQUE,
+		"Name" text NOT NULL,
+		"Password" text NOT NULL,
+		CONSTRAINT "Users_pk" PRIMARY KEY ("id")
+	) WITH (
+	  OIDS=FALSE
+	);`)
+	if err != nil {
+		utils.Logger.Error(err.Error())
+	}
+	_, err = connection.Query(`INSERT INTO users (name, password) values ($1, $2)`, login, password)
+	if err != nil {
+		utils.Logger.Error(err.Error())
+	}
+	cookie := &http.Cookie{Name: "user", Value: login, MaxAge: 300}
+	http.SetCookie(c.Writer, cookie)
+	c.HTML(200, "index.html", gin.H{
+		"Expressions": getExpressionsList(),
+		"User":        cookie.Value,
+	})
+}
+
+func inputExpression(c *gin.Context) {
+	var err error
+	_, err = connection.Query(`CREATE TABLE IF NOT EXISTS "Expression" (
+		"id" serial NOT NULL,
+		"StringVersion" text NOT NULL,
+		"Status" text NOT NULL,
+		"Answer" text NOT NULL,
+		"StartDate" TIMESTAMP WITH TIME ZONE NOT NULL,
+		"EndDate" TIMESTAMP WITH TIME ZONE NOT NULL,
+		CONSTRAINT "Expression_pk" PRIMARY KEY ("id")
+	) WITH (
+	  OIDS=FALSE
+	);`)
+	if err != nil {
+		utils.Logger.Error(err.Error())
+	}
+	cookie, err := c.Request.Cookie("user")
+	if err != nil {
+		cookie = &http.Cookie{Name: "user", Value: "", MaxAge: 300}
+	}
+	http.SetCookie(c.Writer, cookie)
+	c.HTML(200, "index.html", gin.H{
+		"Expressions": getExpressionsList(),
+		"User":        cookie.Value,
+	})
+}
+func getExpressionsList() []Expression {
+	rows, err := connection.Query(`SELECT * FROM "Expression"`)
+	if err != nil {
+		utils.Logger.Error(err.Error())
+	}
+	defer rows.Close()
+	expressions := []Expression{}
+	for rows.Next() {
+		validExpression := new(Expression)
+		rows.Scan(
+			&validExpression.Id,
+			&validExpression.StringVersion,
+			&validExpression.Status,
+			&validExpression.Answer,
+			&validExpression.StartDate,
+			&validExpression.EndDate,
+		)
+		now := time.Now()
+		if validExpression.EndDate.Before(now) {
+			if validExpression.Answer != "not found" {
+				validExpression.Status = "ok"
+			} else {
+				validExpression.Status = "cancel"
+			}
+		}
+		_, err = connection.Query(`UPDATE "Expression" SET "StringVersion"=$2, "Status"=$3, "Answer"=$4, "StartDate"=$5, "EndDate"=$6
+			WHERE "id"=$1`,
+			validExpression.Id,
+			validExpression.StringVersion,
+			validExpression.Status,
+			validExpression.Answer,
+			validExpression.StartDate,
+			validExpression.EndDate,
+		)
+		if err != nil {
+			utils.Logger.Error(err.Error())
+		}
+		expressions = append(expressions, *validExpression)
+	}
+	return expressions
+}
+func createExpression(c *gin.Context) {
+	now := time.Now()
+	var newExpression Expression
+	mathExpression := c.PostForm("math")
+	utils.Logger.Info(mathExpression)
+	endDate := getTime(mathExpression, now)
+	newExpression = Expression{
+		Id:            0,
+		StringVersion: mathExpression,
+		Status:        "ok",
+		StartDate:     now,
+		EndDate:       endDate,
+	}
+	expr, err := eval.ParseString(newExpression.StringVersion, "")
+	if err != nil {
+		newExpression.Status = "cancel"
+	} else {
+		answer, err := expr.EvalToInterface(nil)
+		if err != nil || answer == nil {
+			newExpression.Answer = "not found"
+		} else {
+			newExpression.Answer = fmt.Sprint(answer)
+		}
+	}
+	newExpression.Status = "process"
+	expressions := []Expression{}
+	if mathExpression != "" {
+		_, err := connection.Query(`INSERT INTO "Expression"("StringVersion", "Status", "Answer", "StartDate", "EndDate")
+		VALUES($1, $2, $3, $4, $5)`,
+			newExpression.StringVersion,
+			newExpression.Status,
+			newExpression.Answer,
+			newExpression.StartDate,
+			newExpression.EndDate,
+		)
+		if err != nil {
+			utils.Logger.Error(err.Error())
+		}
+		rows, err := connection.Query(`SELECT * FROM "Expression"`)
+		if err != nil {
+			utils.Logger.Error(err.Error())
+		}
+		for rows.Next() {
+			validExpression := new(Expression)
+			rows.Scan(
+				&validExpression.Id,
+				&validExpression.StringVersion,
+				&validExpression.Status,
+				&validExpression.Answer,
+				&validExpression.StartDate,
+				&validExpression.EndDate,
+			)
+			expressions = append(expressions, *validExpression)
+		}
+		rows.Close()
+	}
+	cookie, err := c.Request.Cookie("user")
+	if err != nil {
+		cookie = &http.Cookie{Name: "user", Value: "", MaxAge: 300}
+	}
+	http.SetCookie(c.Writer, cookie)
+	c.HTML(200, "index.html", gin.H{
+		"Expressions": expressions,
+		"User":        cookie.Value,
+	})
+}
+
+func showOperatorsTime(c *gin.Context) {
+	c.HTML(200, "operators.html", gin.H{
+		"addition":       mapOperatorsTime["+"],
+		"substraction":   mapOperatorsTime["-"],
+		"multiplication": mapOperatorsTime["*"],
+		"division":       mapOperatorsTime["/"],
+	})
+}
+
+func replaceOperatorsTime(c *gin.Context) {
+	mapOperatorsTime["+"], _ = strconv.Atoi(c.PostForm("addition"))
+	mapOperatorsTime["-"], _ = strconv.Atoi(c.PostForm("substraction"))
+	mapOperatorsTime["*"], _ = strconv.Atoi(c.PostForm("multiplication"))
+	mapOperatorsTime["/"], _ = strconv.Atoi(c.PostForm("division"))
+	c.HTML(200, "operators.html", gin.H{
+		"addition":       mapOperatorsTime["+"],
+		"substraction":   mapOperatorsTime["-"],
+		"multiplication": mapOperatorsTime["*"],
+		"division":       mapOperatorsTime["/"],
+	})
+}
+func operatorsStatic(c *gin.Context) {
+	if c.Request.Method == "POST" {
+		mapOperatorsTime["+"], _ = strconv.Atoi(c.PostForm("addition"))
+		mapOperatorsTime["-"], _ = strconv.Atoi(c.PostForm("substraction"))
+		mapOperatorsTime["*"], _ = strconv.Atoi(c.PostForm("multiplication"))
+		mapOperatorsTime["/"], _ = strconv.Atoi(c.PostForm("division"))
+	}
+	c.HTML(200, "static_operators.html", gin.H{
+		"addition":       mapOperatorsTime["+"],
+		"substraction":   mapOperatorsTime["-"],
+		"multiplication": mapOperatorsTime["*"],
+		"division":       mapOperatorsTime["/"],
+	})
+}
+func getExpression(c *gin.Context) {
+	var expression Expression
+	connection.QueryRow(`SELECT * FROM "Expression" WHERE "id"=$1`, c.Param("id")).Scan(
+		&expression.Id,
+		&expression.StringVersion,
+		&expression.Status,
+		&expression.Answer,
+		&expression.StartDate,
+		&expression.EndDate,
+	)
+	var answer any
+	var err error
+	expr, err := eval.ParseString(expression.StringVersion, "")
+	if err != nil {
+		expression.Status = "cancel"
+		c.JSON(400, expression)
+		return
+	}
+	now := time.Now()
+	if expression.EndDate.Before(now) {
+		answer, err = expr.EvalToInterface(nil)
+		if err != nil || answer == nil {
+			expression.Status = "cancel"
+			c.JSON(400, expression)
+			return
+		}
+		expression.Answer = fmt.Sprint(answer)
+		expression.Status = "ok"
+	}
+	c.JSON(200, expression)
+}
+func getTime(expression string, now time.Time) time.Time {
+	var seconds int
+	for _, symbol := range expression {
+		if string(symbol) == "-" || string(symbol) == "+" || string(symbol) == "/" || string(symbol) == "*" {
+			seconds += mapOperatorsTime[string(symbol)]
+		}
+	}
+	return now.Add(time.Duration(seconds) * time.Second)
+}
